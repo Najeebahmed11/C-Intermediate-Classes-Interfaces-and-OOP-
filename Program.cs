e73783df198c0aaac584b35300e83213fddcb66a
Najeeb Ahmed <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-15 10:18:08 +0500

    7 introduction to classes
    classes are building block of software apps
    app consist of multiple classes
    anatomy of class: data and behavior
    UML representing classes
    object talk to each other at run time so we get behavior from the apps
    the public is access modifiers
    inter of naming class we use pascal case
    the object can be created in  2 ways
    Person person=new Person();
    var person=new Person();
    class members:
    instance=acceible from an object
    static=accieble from class;(console.writeline());
    why do we use static:the concept that are singleton,DateTime now(),console.writeline()
    we use static after the access modifiers

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp2.Maths;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Channels;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    public class Person
+    {
+        public string name;
+        public void introduction(string me)
+        {
+            Console.WriteLine("hi {0},I am {1}", me, name);
+        }
+        public static Person parse(string str)
+        {
+            var person = new Person();
+            person.name = str;
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.parse("ahmed");
+            person.introduction("najeeb");
+        }
+
+    }
+}
+
+
+
+

861d9e3df58ae48613ae2f37bc036fb68e570d05
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-15 17:13:22 +0500

    10 Methods(theory)

--- ConsoleApp1/csharpintermediate/Program.cs
+++ ConsoleApp1/csharpintermediate/Program.cs
@@ -6,8 +6,25 @@ namespace csharpintermediate
     {
         static void Main(string[] args)
         {
+ 
             Console.WriteLine("Hello World!");
+/*
+signature of method
+it consist of name number and type of its parameter
+overloading
+method having same name but different signature
+params
+public class calculator
+        {
+            public int Add(params int[] numbers) { }
+        }
+        var result = calculator.add(new int[] { 1, 2, 3,, 3 });
+        var result = calculator.add(1, 2, 3, 4);
+        the ref modifier
+        the out modifier can return value
 
+             
+             */
         }
     }
 }

1b917091051e6283a5ee90de5c82b0fccc40548d
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-16 15:28:18 +0500

    Completed Lesson 13 Propeties

---
+++ ConsoleApp1/csharpintermediate/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Dynamic;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name{ get; set;}
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public DateTime Birthdate{ get;private set; }
+        public int age
+        {
+            get
+            {
+                var timespan= DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
+                return years;
+            }
+        }
+    }
+}
--- ConsoleApp1/csharpintermediate/Program.cs
+++ ConsoleApp1/csharpintermediate/Program.cs
@@ -4,27 +4,15 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-    public class Person
-    {
-        private DateTime _birthdate;
-        public void setBirthdate(DateTime Birthdate)
-        {
-            _birthdate = Birthdate;
 
-        }
-        public DateTime getBirthdate()
-        {
-            return _birthdate;
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.setBirthdate(new DateTime(1997,1,1));
-            Console.WriteLine( person.getBirthdate());
+            var age = new Person(new DateTime(1997, 8, 14));
+            
+            Console.WriteLine(age.age);
         }
+
     }
 }

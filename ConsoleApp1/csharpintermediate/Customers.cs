7a33eaefe81e0cca73961c4923dba656ae7a4af0
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-16 09:20:22 +0500

    Completed Lesson 11 Fields

---
+++ ConsoleApp1/csharpintermediate/Customers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Customers
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders= new List<Order>();
+        
+        public Customers(int id)
+        {
+            this.Id = id;
+
+        }
+        public Customers(int id, string name)
+            :this(id)
+        {
+            this.Name = name;
+
+        }
+        public void Promote()
+        {
+           // Orders = new List<Order>();
+        }
+    }
+}
--- ConsoleApp1/csharpintermediate/Program.cs
+++ ConsoleApp1/csharpintermediate/Program.cs
@@ -1,42 +1,21 @@
 ﻿using System;
+using System.Drawing;
+using System.Linq;
 
-namespace csharpintermediate
+namespace ConsoleApp1
 {
-    partial class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            //out classifier
-            // var st = int.Parse("aab");
-            int numm;
-            var ezz = int.TryParse("aab",out numm);
-            if (ezz = true)
-            {
-                Console.WriteLine("conversion failed");
-            }
-        }
-        static void useParams()
-        {
-            var cal = new calculator();
-            Console.WriteLine(cal.Add(1, 2, 3, 4));
+            var cus = new Customers(1, "najeeb");
+            cus.Orders.Add(new Order());
+            cus.Orders.Add(new Order());
+            cus.Orders.Add(new Order());
+            cus.Orders.Add(new Order());
+            cus.Promote();
+            Console.WriteLine(cus.Orders.Count());
 
-        }
-        static void usePoints()
-        {
-            try
-            {
-                Point point = new Point(10, 20);
-                point.Move(new Point(40, 44));
-                point.Move(null);
-                Console.WriteLine(point.X);
-                Console.WriteLine(point.Y);
-
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("there is exception");
-            }
         }
     }
 }

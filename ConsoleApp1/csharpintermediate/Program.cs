5cb311b5422c048e5493b65f486f4c66a4782bff
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-16 10:02:20 +0500

    Completed Lesson 12 Access Modifiers

--- ConsoleApp1/csharpintermediate/Program.cs
+++ ConsoleApp1/csharpintermediate/Program.cs
@@ -4,17 +4,27 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
+    public class Person
+    {
+        private DateTime _birthdate;
+        public void setBirthdate(DateTime Birthdate)
+        {
+            _birthdate = Birthdate;
+
+        }
+        public DateTime getBirthdate()
+        {
+            return _birthdate;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-           //access modifier is way to access to a class or its member
-           //why?for safety of pgms
-           //encapsulation:we dont want classes to know detail of other classes;
-           //encapsulation:define field as private and getter setter method as public
-           //setter and getter can check strings values
-           //in camel case private fields should start with underline _name eg
-           //
+            var person = new Person();
+            person.setBirthdate(new DateTime(1997,1,1));
+            Console.WriteLine( person.getBirthdate());
         }
     }
 }

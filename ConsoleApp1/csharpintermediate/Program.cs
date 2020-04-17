d0d5451aa1d31907ff6619f92f5df55e6296dc94
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-17 12:33:23 +0500

    Complted Lesson 22 AccessModifier(Theory)

---
+++ ConsoleApp1/csharpintermediate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpintermediate
+{
+    class Program
+    {
+        using System.Collections.Generic;
+
+namespace MethodOverriding
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Access Modifiers
+                //what we see from outside is called public interface
+                //private modifiers are only accessible from class
+                //protected is only accessible from class and its derived class
+                //protected break encapsulation
+                //internal is often used with classes not members
+                //class is only visible in inside assembly while outside assembly it is not visible
+                //proteted-internal:it is used with method,only accessible from same assembly or derived classes.
+                //
+            }
+        }
+    }
+
+}
+}

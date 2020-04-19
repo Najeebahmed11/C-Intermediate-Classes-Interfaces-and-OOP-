c78a51f1b932a87195b2b52b9dc5584f295f27e4
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-19 08:51:02 +0500

    Completed Lesson 39 Exersice

--- ConsoleApp1/csharpintermediate/Program.cs
+++ ConsoleApp1/csharpintermediate/Program.cs
@@ -1,20 +1,174 @@
-﻿namespace csharpintermediate
+﻿using System.Collections.Generic;
+
+namespace csharpintermediate
 {
 
     public class Program
+
     {
+
         static void Main(string[] args)
+
         {
-            //completed Revision of section 6
 
-            var encoder = new VideoEncoder();
-            encoder.RegisterNotificationChannel(new MailNotificationChannel());
-            encoder.RegisterNotificationChannel(new SmsNotificationChannel());
-            encoder.Encode(new Video());
+            //Creating new WorkFlow object and its empty list of steps.
 
+            var workflow = new WorkFlow();
+
+
+
+            //Adding new steps to workflow object created.
+
+            workflow.AddStep(new Step("step 1"));
+
+            workflow.AddStep(new Step("step 2"));
+
+            workflow.AddStep(new Step("step 3"));
+
+
+
+            //Running the workflow.
+
+            var workflowengine = new WorkFlowEngine();
+
+            workflowengine.Run(workflow);
 
         }
 
-    }       
+    }
+
 }
+
+
+
+//Workflow Engine_Controller
+
+
+
+public class WorkFlowEngine
+
+{
+
+    //private readonly WorkFlow _workFlow;
+
+
+
+    //public WorkFlowEngine( WorkFlow workFlow)
+
+    //{
+
+    //    this._workFlow = workFlow;
+
+    //}
+
+
+
+    public void Run(WorkFlow WorkFlow)
+
+    {
+
+        WorkFlow.ExcecuteWorkFLow();
+
+    }
+
+}
+
+
+
+//Workflow Entity
+
+public class WorkFlow
+
+{
+
+    public int IdWorkFlow { get; set; }
+
+    public string Description { get; set; }
+
+    private readonly IList<IStep> _steps;
+
+
+
+    public WorkFlow()
+
+    {
+
+        _steps = new List<IStep>();
+
+    }
+
+
+
+    public void AddStep(IStep step)
+
+    {
+
+        _steps.Add(step);
+
+    }
+
+
+
+    public void ExcecuteWorkFLow()
+
+    {
+
+        foreach (var step in _steps)
+
+        {
+
+            step.Execute();
+
+        }
+
+    }
+
+}
+
+
+
+//Step(Activity) Entity
+
+public class Step : IStep
+
+{
+
+    public int StepID { get; set; }
+
+    public string StepDescription { get; set; }
+
+
+
+    public Step(string stepDescription)
+
+    {
+
+        StepDescription = stepDescription;
+
+    }
+
+
+
+    public void Execute()
+
+    {
+
+        System.Console.WriteLine("Step excecuted - Description:{0} ", StepDescription);
+
+    }
+
+}
+
+//Interface IStep definition
+
+public interface IStep
+
+{
+
+    void Execute();
+
+}
+
+
+
 

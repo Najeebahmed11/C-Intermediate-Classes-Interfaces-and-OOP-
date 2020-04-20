c26b2dfb140929bb2e1acc02a3ad74ac922e19d4
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-20 10:54:40 +0500

    Completed Lesson 15. Asynchronous Programming with Async / Await

---
+++ ConsoleApp1/Asynch/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asynch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //button click is handler
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // DownloadHtmlAsynch("https://github.com/Najeebahmed11/C-Intermediate-Classes-Interfaces-and-OOP-/commits/master");
+            var getHtml=  GetHtmlAsync("https://mail.google.com/mail/u/0/#inbox");
+            MessageBox.Show("waiting for task to complete");
+            var html = await getHtml;
+            MessageBox.Show(html.Substring(0, 10));
+
+        }
+      
+        //task is an object that encapsulate the state of asynch opperation
+        //if your method return a string than use generic version
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webclient = new WebClient();
+            return await webclient.DownloadStringTaskAsync(url);
+        }
+        public async Task DownloadHtmlAsynch (string url)
+        {
+            var webClient = new WebClient();
+            var html = await webClient.DownloadStringTaskAsync(url);
+            using (var stramwriter = new StreamWriter("D:\\internship\\Day9\\result.html"))
+            {
+                await stramwriter.WriteAsync(html);
+
+            }
+        }
+        public void DownloadHtml(string url)
+        {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString(url);
+            using (var stramwriter = new StreamWriter("D:\\internship\\result.html"))
+            {
+                stramwriter.Write(html);
+
+            }
+        }
+    }
+}
--- ConsoleApp1/csharpadvanvce/Program.cs
+++ ConsoleApp1/csharpadvanvce/Program.cs
@@ -8,26 +8,17 @@ namespace csharpadvanvce
     {
         public static void Main(string[] args)
         {
-            //exception is a class
-            //stack_trace:the sequence of methods that were called untill exception was thrown
-            //in exception handling source represent dll or assembly
-            //we should put exception from most specific to most generic
-            //in this way we can handle exceptions
-            //Idosposible use desposible to manage resources
-            //dispose()is usually use to close file
-            //databases,network connection,graphics,make sure to use dispose
-            //using block have its own dispose() insted of calling finally block
-            //entity framewok usualyy have inner exceptions
-
-            try
-            {
-                var api = new YouTubeApi();
-                var videos = api.GetVideos("mosh");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+           //Asynchronous Programming:
+           //in sync prog programme is executed line by line one at a time
+           //when func is called programme execution have to wait untill function return something
+           //e.g when we access a database,a website
+           //Async rog:a function is called programme execution to cintinues to next line without waiting for func to complete
+           //we have to provide callback in that function
+           //async improves responsess of your app
+           //e.g,windows media player,web browser
+           //use:working with files,databases,images
+           //approch:multi-threading,callback,async/await
+           //
         }
     }
 }

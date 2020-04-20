70b298b857f30eb4868f3c45c69f70e3615b3cca
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-20 11:26:57 +0500

    Completed Revision Lec 6 Generics

--- ConsoleApp1/csharpadvanvce/Program.cs
+++ ConsoleApp1/csharpadvanvce/Program.cs
@@ -8,16 +8,11 @@ namespace csharpadvanvce
     {
         public static void Main(string[] args)
         {
-           //Asynchronous Programming:
-           //in sync prog programme is executed line by line one at a time
-           //when func is called programme execution have to wait untill function return something
-           //e.g when we access a database,a website
-           //Async rog:a function is called programme execution to cintinues to next line without waiting for func to complete
-           //we have to provide callback in that function
-           //async improves responsess of your app
-           //e.g,windows media player,web browser
-           //use:working with files,databases,images
-           //approch:multi-threading,callback,async/await
+           //revision
+           //lec 6 Generics
+           //object list can also be used but every time it should be boxed to store values
+           //this is perfomance penality
+           //system.collection.generic...>classes
            //
         }
     }
